c3fb1638cb2824b0186ae14d1111c6448509402f
Kingsley Ifijeh <kinxly@gmail.com>
2025-04-30 16:10:47 +0100

    fix: removed comments in context loader

--- src/context/progress-provider.tsx
+++ src/context/progress-provider.tsx
@@ -4,13 +4,7 @@ import { ProgressProvider } from "@bprogress/next/app";
 
 export const ProgressProviders = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ProgressProvider
-      height="4px"
-      // color={`#${Math.floor(Math.random() * 16_777_215).toString(16)}`}
-      color={`#FF0000`}
-      options={{ showSpinner: false }}
-      shallowRouting
-    >
+    <ProgressProvider height="4px" color={`#FF0000`} options={{ showSpinner: false }} shallowRouting>
       {children}
     </ProgressProvider>
   );
