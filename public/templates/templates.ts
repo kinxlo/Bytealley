01d25c66041ec3641c3f01d2f4ebd100595c760f
Kingsley Ifijeh <kinxly@gmail.com>
2025-07-14 14:54:05 +0100

    fix: cleaned up template-1

--- public/templates/templates.ts
+++ public/templates/templates.ts
@@ -16,15 +16,15 @@ export const templatesMetadata = [
       { id: "page-3", name: "Thank you", file: "thank-you.html" },
     ],
   },
-  // {
-  //   id: "multi-step-sale",
-  //   thumbnail: "/images/template-6.png",
-  //   pages: [
-  //     { id: "page-1", name: "Home", file: "index.html" },
-  //     { id: "page-2", name: "Page two", file: "page-2.html" },
-  //     { id: "page-3", name: "Thank you", file: "thank-you.html" },
-  //   ],
-  // },
+  {
+    id: "multi-step-sale",
+    thumbnail: "/images/template-6.png",
+    pages: [
+      { id: "page-1", name: "Home", file: "index.html" },
+      { id: "page-2", name: "Page two", file: "page-2.html" },
+      { id: "page-3", name: "Thank you", file: "thank-you.html" },
+    ],
+  },
   {
     id: "sales",
     thumbnail: "/images/template-3.png",
